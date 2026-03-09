16bf25a7e0b7bc820f4a779c8ffb03e15b49a1b2
karencardona2 <karencardona1138899@correo.itm.edu.co>
2026-03-08 22:28:01 -0500

    Project Finished

---
+++ logic/Is-Numer/Program.cs
@@ -0,0 +1,12 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Ingrese numero: ");
+var numberString = Console.ReadLine();
+var numberInt = int.Parse(numberString!);
+if  (numberInt % 2 == 0)
+{
+    Console.WriteLine("El numero es par");
+}
+else
+{
+    Console.WriteLine("El numero es impar");
+}
---
+++ proyect/Actividad31/LowerTriangularAreaMatrix/Program.cs
@@ -0,0 +1,82 @@
+﻿using System;
+
+namespace MatrixExercises
+{
+    public class LowerTriangularMatrix
+    {
+        private int[,] _data;
+        private int _order;
+
+        public int Order => _order;
+
+        public LowerTriangularMatrix(int n)
+        {
+            _order = n;
+            _data = new int[n, n];
+            FillMatrix();
+        }
+
+        private void FillMatrix()
+        {
+            for (int row = 0; row < _order; row++)
+            {
+                for (int col = 0; col < _order; col++)
+                {
+                    _data[row, col] = row + col;
+                }
+            }
+        }
+
+        public int GetValue(int row, int col) => _data[row, col];
+
+        public bool IsLowerTriangular(int row, int col)
+        {
+            return row >= col;
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Ingrese orden de la matriz: ");
+            
+            if (int.TryParse(Console.ReadLine(), out int n) && n > 0)
+            {
+                LowerTriangularMatrix matrix = new LowerTriangularMatrix(n);
+
+                Console.WriteLine("\nMATRIZ COMPLETA:");
+                PrintMatrix(matrix, showFull: true);
+
+                Console.WriteLine("\nTRIANGULAR INFERIOR:");
+                PrintMatrix(matrix, showFull: false);
+            }
+            else
+            {
+                Console.WriteLine("Error: Por favor, ingrese un número entero válido.");
+            }
+
+            Console.WriteLine("\nPresione cualquier tecla para salir...");
+            Console.ReadKey();
+        }
+
+        static void PrintMatrix(LowerTriangularMatrix matrix, bool showFull)
+        {
+            for (int i = 0; i < matrix.Order; i++)
+            {
+                for (int j = 0; j < matrix.Order; j++)
+                {
+                    if (showFull || matrix.IsLowerTriangular(i, j))
+                    {
+                        Console.Write(matrix.GetValue(i, j).ToString().PadLeft(4));
+                    }
+                    else
+                    {
+                        Console.Write("    ");
+                    }
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
